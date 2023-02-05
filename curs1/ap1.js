ca05607214a8ace215aa0cc590501b7008073833
Igor Nistor <igor.nistor@hotmail.com>
2023-02-05 02:35:02 +0200

    JS curs 1

---
+++ curs1/ap1.js
@@ -0,0 +1,7 @@
+alert('salut JavaScript!')
+window.alert('salut')
+confirm('vreau sa invat JS!')
+/* console.log('mesaj in consola')
+ console.info('info status')
+ console.error('mesaj de eroare')
+ */
